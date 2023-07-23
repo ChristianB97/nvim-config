0c999117b8c2405ba2f4e8c2c7a9b769c903a4b8
Christian Blanz <christian.blanz@outlook.de>
2023-07-23 02:07:36 +0200

    ADD of lua lsp UPDATE of formatting ADD of readme

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -4,7 +4,7 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
+		-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 
 	use({
@@ -34,6 +34,28 @@ return require("packer").startup(function(use)
 	use("mbbill/undotree")
 	use("tpope/vim-fugitive")
 
-        use("sbdchd/neoformat")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("sbdchd/neoformat")
         use {"akinsho/toggleterm.nvim", tag = '*'}
 end)
