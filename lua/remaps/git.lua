69bcfafd5ef941a8a5706e7df9b092f6dddf8a70
Christian Blanz <christian.blanz@outlook.de>
2023-07-30 23:12:16 +0200

    FIX of push bug

--- lua/remaps/git.lua
+++ lua/remaps/git.lua
@@ -30,9 +30,21 @@ vim.keymap.set("n", "<leader>gp", function()
     local current_branch = handle:read("*a"):gsub("\n", "")
     handle:close()
 
-    local handle_pushed = io.popen('git log origin/' .. current_branch .. '..' .. current_branch)
-    local not_pushed = handle_pushed:read("*a")
-    handle_pushed:close()
+    local handle_upstream = io.popen('git rev-parse --abbrev-ref --symbolic-full-name @{upstream} 2> /dev/null')
+    local upstream_branch = handle_upstream:read("*a")
+    handle_upstream:close()
+
+    local not_pushed = ""
+
+    if upstream_branch ~= "" then
+        local handle_pushed = io.popen('git cherry')
+        not_pushed = handle_pushed:read("*a")
+        handle_pushed:close()
+    else
+        local handle_pushed = io.popen('git log --branches --not --remotes')
+        not_pushed = handle_pushed:read("*a")
+        handle_pushed:close()
+    end
 
     if not_pushed == "" then
         print("Nothing to push (≖_≖ )")
@@ -42,7 +54,7 @@ vim.keymap.set("n", "<leader>gp", function()
     vim.fn.jobstart('git push origin ' .. current_branch, {
         on_stderr = function(j, data, event)
             local error_msg = data[1]
-            if string.match(error_msg, 'has no upstream branch') then
+            if string.match(error_msg, 'no upstream branch') then
                 vim.fn.jobstart('git push --set-upstream origin ' .. current_branch)
             end
         end,
@@ -55,7 +67,48 @@ vim.keymap.set("n", "<leader>gp", function()
         end
     })
 end)
+vim.keymap.set("n", "<leader>gp", function()
+    local handle = io.popen('git rev-parse --abbrev-ref HEAD')
+    local current_branch = handle:read("*a"):gsub("\n", "")
+    handle:close()
 
+    local handle_upstream = io.popen('git rev-parse --abbrev-ref --symbolic-full-name @{upstream} 2> /dev/null')
+    local upstream_branch = handle_upstream:read("*a")
+    handle_upstream:close()
+
+    local not_pushed = ""
+
+    if upstream_branch ~= "" then
+        local handle_pushed = io.popen('git cherry')
+        not_pushed = handle_pushed:read("*a")
+        handle_pushed:close()
+    else
+        local handle_pushed = io.popen('git log --branches --not --remotes')
+        not_pushed = handle_pushed:read("*a")
+        handle_pushed:close()
+    end
+
+    if not_pushed == "" then
+        print("Nothing to push (≖_≖ )")
+        return
+    end
+
+    vim.fn.jobstart('git push origin ' .. current_branch, {
+        on_stderr = function(j, data, event)
+            local error_msg = data[1]
+            if string.match(error_msg, 'no upstream branch') then
+                vim.fn.jobstart('git push --set-upstream origin ' .. current_branch)
+            end
+        end,
+        on_exit = function(j, return_val, event)
+            if return_val == 0 then
+                print("Pushed ٩( ᐛ )و")
+            else
+                print("(ノಠ益ಠ)ノ彡PUSH FAILED")
+            end
+        end
+    })
+end)
 
 
 vim.keymap.set("n", "<leader>gb", function()
