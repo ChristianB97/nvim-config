ef2f26a581b065e7dbc2fcf2f1180d1b24e521b4
Christian Blanz <christian.blanz@outlook.de>
2023-08-03 03:36:33 +0200

    REFACTOR of terminal shortcuts by adding a loop ADD of vertical terminal shortcut

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -6,27 +6,23 @@ require'toggleterm'.setup({
     winbar = {enabled = false}
 });
 
-function _G.execute_term(num) require("toggleterm").exec("", num) end
-vim.api.nvim_set_keymap('n', '\\1', '<cmd>lua execute_term(1)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\2', '<cmd>lua execute_term(2)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\3', '<cmd>lua execute_term(3)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\4', '<cmd>lua execute_term(4)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\5', '<cmd>lua execute_term(5)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\6', '<cmd>lua execute_term(6)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\7', '<cmd>lua execute_term(7)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\8', '<cmd>lua execute_term(8)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\9', '<cmd>lua execute_term(9)<CR>',
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '\\0', '<cmd>lua execute_term(10)<CR>',
-                        {noremap = true, silent = true})
+function _G.execute_term(num, direction)
+    vim.cmd("ToggleTerm " .. num .. " size=85 direction=" .. direction)
+end
+
+function _G.execute_multiple_terminals(direction, amount)
+    for i = 0, amount - 1 do execute_term(i, direction) end
+end
+
+for i = 0, 9 do
+    vim.api.nvim_set_keymap('n', '\\' .. i,
+                            '<cmd>lua execute_term(' .. i .. ', "tab")<CR>',
+                            {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('n', '\\v' .. i, '<cmd>lua execute_term(' .. i ..
+                                ', "vertical")<CR>',
+                            {noremap = true, silent = true})
+end
+
 vim.api.nvim_set_keymap('n', '\\\\', ':ToggleTerm<CR>',
                         {noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '\\\\', '<C-\\><C-n>:ToggleTerm<CR>',
